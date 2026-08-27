7d1bffbf38a2b983bd37996a84d64f2556701a53
umaparmila123 <324103383056@gvpce.ac.in>
2026-08-27 22:51:44 +0530

    Fix wildcard route syntax for SPA fallback

--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // SPA client-side routing fallback
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
